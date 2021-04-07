cb9963acd83fdd9aaf3cdc4b02ac04a162271b73
Alyssa <aebowden36@gmail.com>
2021-04-07 17:45:28 -0400

    made progress on pypoll

--- pybank/main.py
+++ pybank/main.py
@@ -15,7 +15,7 @@ os.chdir(pybank_path)
 #lists to store data in
 profit = []
 monthlyChange = []
-date = []
+month = []
 
 monthTotal = 0
 netTotal = 0
@@ -35,33 +35,57 @@ with open("PyBank_budget_data.csv","r") as csvfile:
         for row in csvreader:
             
             #find the total number of months in the data
+            #store the month in empty list
             monthTotal += 1
-            date.append(row[0])
+            month.append(row[0])
+            
+            #store the profit information in empty list
             #find the total profit
             profit.append(row[1])
-            netTotal += float(row[1])
+            netTotal += int(row[1])
 
+            #calculate the monthly profit changes
             finalProfit = int(row[1])
             monthlyChangeProfit = finalProfit - initialProfit
+            
+            #store monthly changes in empty list
             monthlyChange.append(monthlyChangeProfit)
             
             totalChange += monthlyChangeProfit
             initialProfit = finalProfit
-
+            
+            #calculate the average profit change
             averageChange = (totalChange/monthTotal)
-
+            #find the greatest increase and decrease
             increase = max(monthlyChange)
             decrease = min(monthlyChange)
-
-            increaseDate = date[monthlyChange.index(increase)]
-            decreaseDate = date[monthlyChange.index(decrease)]
-
+            #find the months/years of the greatest increase and greatest decrease
+            increaseDate = month[monthlyChange.index(increase)]
+            decreaseDate = month[monthlyChange.index(decrease)]
+#print to the terminal
 print("---------------------")
 print("Financial Analysis:")
 print("---------------------")
 print(f"Total months: {monthTotal}")
 print(f"Total: ${netTotal}")
 print(f"Average Change: ${averageChange}")
-print(f"Greatest Increase: {increaseDate} ${increase}")
-print(f"Greatest Decrease: {decreaseDate} ${decrease}")
+print(f"Greatest Increase in Profits: {increaseDate} ${increase}")
+print(f"Greatest Decrease in Profits: {decreaseDate} ${decrease}")
 print("---------------------")
+
+
+# pybank_path_write = os.path.join(dir_path, 'analysis')
+# print(pybank_path_write)
+# print(type(pybank_path_write))
+# os.chdir(pybank_path_write)
+
+# with open("pybank_analysis.txt", "w") as text:
+#     text.write("--------------------- \n")
+#     text.write("Financial Analysis: \n")
+#     text.write("--------------------- \n")
+#     text.write(f"Total months: {monthTotal} \n")
+#     text.write(f"Total: ${netTotal} \n")
+#     text.write(f"Average Change: ${averageChange} \n")
+#     text.write(f"Greatest Increase in Profits: {increaseDate} ${increase} \n")
+#     text.write(f"Greatest Decrease in Profits: {decreaseDate} ${decrease} \n")
+#     text.write("--------------------- \n")
--- pypoll/main.py
+++ pypoll/main.py
@@ -12,22 +12,59 @@ print(pypoll_path)
 print(type(pypoll_path))
 os.chdir(pypoll_path)
 
-candidate = []
-
+votesTotal = 0
+#create empty lists
+candidateList = []
+candidateUnique = []
+candidateVoteTotal = []
+candidateVotePercent = []
 with open("PyPoll_data.csv","r") as csvfile:
         
         csvreader = csv.reader(csvfile, delimiter = ',')
-
-        print(csvreader)
     #Skip header
         next(csvreader)
-        votesTotal = 0
+        
         
         #read each row of data after the header
         for row in csvreader:
-            #print(row[0])
+            #find total number of votes
             votesTotal += 1
-            if row[2] != row[2]:
-                candidate.append(row[2])
+
+            candidateList.append(row[2])
+
+        for x in set(candidateList):
+            candidateUnique.append(x)
+            y = candidateList.count(x)
+            candidateVoteTotal.append(y)
+            z = (y/votesTotal)*100
+            candidateVotePercent.append(z)  
+        winnerVotes = max(candidateVoteTotal)
+        winner = candidateUnique[candidateVoteTotal.index(winnerVotes)]
+
+print("---------------------")
+print("Election Results:")
+print("---------------------")
 print(f"Total votes: {votesTotal}")
-print(candidate)
+print("---------------------")
+for i in range(len(candidateUnique)):
+    print(f"{candidateUnique[i]}: {round(candidateVotePercent[i], 2)}% ({candidateVoteTotal[i]})")
+print("---------------------")
+print(f"The winner is: {winner}")
+print("---------------------")
+
+# pypoll_path_write = os.path.join(dir_path, 'analysis')
+# print(pypoll_path_write)
+# print(type(pypoll_path_write))
+# os.chdir(pypoll_path_write)
+
+# with open("pypoll_analysis.txt", "w") as text:
+#     text.write("--------------------- \n")
+#     text.write("Election Results: \n")
+#     text.write("--------------------- \n")
+#     text.write(f"Total votes: {votesTotal} \n")
+#     text.write("--------------------- \n")
+#     for i in range(len(candidateUnique)):
+#         text.write(f"{candidateUnique[i]}: {round(candidateVotePercent[i], 2)}% ({candidateVoteTotal[i]}) \n")
+#     text.write("--------------------- \n")
+#     text.write(f"The winner is: {winner} \n")
+#     text.write("--------------------- \n")
